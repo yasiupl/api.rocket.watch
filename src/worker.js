a0782cc2f995e35011e828140f876442d992f27b
yasiupl <contact@yasiu.pl>
2020-06-26 02:55:34 +0200

    fix broken function references

--- src/worker.js
+++ src/worker.js
@@ -20,15 +20,15 @@ setInterval(function () {
 
 function loadPrimary() {
   try {
-    RocketWatch.loadQuery("/launch/next/4?status=1,2,5,6", function (next) {
+    RocketWatch("/launch/next/4?status=1,2,5,6", function (next) {
       for (var i in next.launches) {
         //cache next 3 launches and notify if needed
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + next.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + next.launches[i].id + "&format=live");
       }
     })
-    RocketWatch.loadQuery("/launch?limit=4&sort=desc&mode=summary&status=3,4,7", function (prev) {
+    RocketWatch("/launch?limit=4&sort=desc&mode=summary&status=3,4,7", function (prev) {
       for (var i in prev.launches) {
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + prev.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + prev.launches[i].id + "&format=live");
       }
     });
   } catch (e) {
@@ -39,67 +39,67 @@ function loadPrimary() {
 
 function loadSecondary() {
   try {
-    RocketWatch.loadQuery("/agency/121?mode=verbose&format=news");
-    RocketWatch.loadQuery("/launch?limit=200&mode=summary&sort=desc&name=&lsp=121&format=stats", function (d) {
+    RocketWatch("/agency/121?mode=verbose&format=news");
+    RocketWatch("/launch?limit=200&mode=summary&sort=desc&name=&lsp=121&format=stats", function (d) {
       for (var i in d.launches) {
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
       }
     });
-    RocketWatch.loadQuery("/agency/124?mode=verbose&format=news");
-    RocketWatch.loadQuery("/launch?limit=200&mode=summary&sort=desc&name=&lsp=124&format=stats", function (d) {
+    RocketWatch("/agency/124?mode=verbose&format=news");
+    RocketWatch("/launch?limit=200&mode=summary&sort=desc&name=&lsp=124&format=stats", function (d) {
       for (var i in d.launches) {
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
       }
     });
-    RocketWatch.loadQuery("/agency/115?mode=verbose&format=news");
-    RocketWatch.loadQuery("/launch?limit=200&mode=summary&sort=desc&name=&lsp=115&format=stats", function (d) {
+    RocketWatch("/agency/115?mode=verbose&format=news");
+    RocketWatch("/launch?limit=200&mode=summary&sort=desc&name=&lsp=115&format=stats", function (d) {
       for (var i in d.launches) {
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
       }
     });
-    RocketWatch.loadQuery("/agency/63?mode=verbose&format=news");
-    RocketWatch.loadQuery("/launch?limit=200&mode=summary&sort=desc&name=&lsp=63&format=stats", function (d) {
+    RocketWatch("/agency/63?mode=verbose&format=news");
+    RocketWatch("/launch?limit=200&mode=summary&sort=desc&name=&lsp=63&format=stats", function (d) {
       for (var i in d.launches) {
-        RocketWatch.loadQuery("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
+        RocketWatch("/launch?mode=verbose&id=" + d.launches[i].id + "&format=live");
       }
     });
 
-    RocketWatch.loadQuery("/agency?limit=30&islsp=1&offset=0", function (d) {
+    RocketWatch("/agency?limit=30&islsp=1&offset=0", function (d) {
       for (i = 1; (30 * i) - d.total < 30; i++) {
-        RocketWatch.loadQuery("/agency?limit=30&islsp=1&offset=" + (i * 30), function (c) {
+        RocketWatch("/agency?limit=30&islsp=1&offset=" + (i * 30), function (c) {
           for (var j in c.agencies) {
-            RocketWatch.loadQuery("/agency/" + c.agencies[j].id + "?format=news");
+            RocketWatch("/agency/" + c.agencies[j].id + "?format=news");
           }
         });
       }
       for (var i in d.agencies) {
-        RocketWatch.loadQuery("/agency/" + d.agencies[i].id + "?format=news");
+        RocketWatch("/agency/" + d.agencies[i].id + "?format=news");
       }
     });
 
-    RocketWatch.loadQuery("/agency?limit=30&mode=verbose&islsp=1&offset=0", function (d) {
+    RocketWatch("/agency?limit=30&mode=verbose&islsp=1&offset=0", function (d) {
       for (i = 1; (30 * i) - d.total < 30; i++) {
-        RocketWatch.loadQuery("/agency?limit=30&mode=verbose&islsp=1&offset=" + (i * 30), function (c) {
+        RocketWatch("/agency?limit=30&mode=verbose&islsp=1&offset=" + (i * 30), function (c) {
           for (var j in c.agencies) {
-            RocketWatch.loadQuery("/agency/" + c.agencies[j].id + "?format=news");
+            RocketWatch("/agency/" + c.agencies[j].id + "?format=news");
           }
         });
       }
       for (var i in d.agencies) {
-        RocketWatch.loadQuery("/agency/" + d.agencies[i].id + "?format=news");
+        RocketWatch("/agency/" + d.agencies[i].id + "?format=news");
       }
     });
 
-    RocketWatch.loadQuery("/rocket?mode=verbose&limit=27&offset=0", function (d) {
+    RocketWatch("/rocket?mode=verbose&limit=27&offset=0", function (d) {
       for (i = 1; (30 * i) - d.total < 30; i++) {
-        RocketWatch.loadQuery("/rocket?mode=verbose&limit=27&offset=" + (i * 30), function (c) {
+        RocketWatch("/rocket?mode=verbose&limit=27&offset=" + (i * 30), function (c) {
           for (var j in c.rockets) {
-            RocketWatch.loadQuery("/rocket/" + c.rockets[j].id + "?mode=verbose");
+            RocketWatch("/rocket/" + c.rockets[j].id + "?mode=verbose");
           }
         });
       }
       for (var i in d.rockets) {
-        RocketWatch.loadQuery("/rocket/" + d.rockets[i].id);
+        RocketWatch("/rocket/" + d.rockets[i].id);
       }
     });
   } catch (e) {
